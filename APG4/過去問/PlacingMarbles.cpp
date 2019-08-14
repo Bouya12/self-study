355f205ef33ec055b2738bda7252b205a35e686d
bouya12 <tsuredurebouya@gmail.com>
2019-08-14 23:03:03 +0900

    Create ABC081 PlacingMarbles

---
+++ APG4/過去問/PlacingMarbles.cpp
@@ -0,0 +1,18 @@
+// AtCoder Beginner Contest 081
+// A - Placing Marbles
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  
+  int count = 0;
+  for (int i = 0; i < 3; i++) {
+    if (s[i] == '1') {
+      count++;
+    }
+  }
+  
+  cout << count << endl;
+}
