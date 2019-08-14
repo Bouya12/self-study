f4feae6d412a0cfeb754ea6d9785b766bf069818
bouya12 <tsuredurebouya@gmail.com>
2019-08-14 22:50:57 +0900

    Create ABC082_A_RoundUP

---
+++ APG4/過去問/RoundUP_Mean.cpp
@@ -0,0 +1,11 @@
+// AtCoder Beginner Contest 082
+// A - RoundUP the Mean
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b;
+  
+  cin >> a >> b;
+  cout << (a + b + 1) / 2  << endl;
+}
