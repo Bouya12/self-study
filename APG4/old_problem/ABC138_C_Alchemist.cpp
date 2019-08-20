057ce5ea2df62fe1e008062352d48556302204ef
bouya12 <tsuredurebouya@gmail.com>
2019-08-20 21:20:26 +0900

    Modify ABC138 C - Alchemist

--- APG4/old_problem/ABC138_C_Alchemist.cpp
+++ APG4/old_problem/ABC138_C_Alchemist.cpp
@@ -2,39 +2,22 @@
 // C - Alchemist
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main() {
   int n;
   cin >> n;
   
-  int v;
-  vector<float> value(n);
+  vector<float> v(n);
   for (int i = 0; i < n; i++) {
-    cin >> v;
-    value.at(i) = v;
+    cin >> v.at(i);
   }
-  
-  float top_val = 0;
-  float total_val = 0;
-  vector<float> value2(n);
-  for (int i = 0; i < n; i++) {
-    
-    // 配列を複製
-    for (int j = 0; j < n; j++) {
-      value2.at(j) = value.at(j);
-    }
-    
-    float v1;
-    float v2;
-    total_val = value2.at(i);
-    value2.erase(i);
-    while (value2.size() > 1) {
-      total_val = (total_val + value2.at(i)) / 2;
-      value2.erase(0);
-    }
-    
-    if (top_val < total_val) {
-      top_val = total_val;
-    }
+  sort(v.begin(), v.end());
+ 
+  double value = v.at(0);
+  for (int i = 1; i < n; i++) {
+    value += v.at(i);
+    value /= 2;
   }
+ 
+  cout << value << endl;
 }
