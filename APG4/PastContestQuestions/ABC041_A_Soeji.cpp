21e789b27840ac23329c577d7bb0052688732c96
bouya12 <tsuredurebouya@gmail.com>
2019-08-26 23:58:17 +0900

    Create ABC041 A - 添字

---
+++ APG4/PastContestQuestions/ABC041_A_Soeji.cpp
@@ -0,0 +1,13 @@
+// AtCoder Grand Contest 041
+// A - 添字
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  string s;
+  int i;
+  cin >> s;
+  cin  >> i;
+
+  cout << s.at(i - 1) << endl;
+}
