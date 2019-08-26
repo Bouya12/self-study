84a442c6a1640dc7b815a78ff36e839624285ac6
bouya12 <tsuredurebouya@gmail.com>
2019-08-26 23:53:24 +0900

    Create ABC050 A - Addition and Subtraction Easy

---
+++ APG4/PastContestQuestions/ABC050_A_AdditionAndSubtraction.cpp
@@ -0,0 +1,16 @@
+// AtCoder Grand Contest 050
+// A - Addition and Subtraction Easy
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int a, b;
+  char op;
+  cin >> a >> op >> b;
+
+  if (op == '+') {
+    cout << a + b << endl;
+  } else {
+    cout << a - b << endl;
+  }
+}
