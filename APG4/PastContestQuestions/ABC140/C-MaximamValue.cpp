851a6376330c39d278ccbb1dfd1d94aecfedd468
bouya12 <tsuredurebouya@gmail.com>
2019-09-07 22:52:36 +0900

    Create ABC140 C - Maximam Value ※未完成のため要修正

---
+++ APG4/PastContestQuestions/ABC140/C-MaximamValue.cpp
@@ -0,0 +1,22 @@
+// AtCoder Beginner Contest 140
+// C - Maximam Value
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<int> B(N - 1);
+  for (int i = 0; i < N; i++) {
+    cin >> B.at(i);
+  }
+
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) {
+    
+  }
+
+  int ans;
+  cout << ans << endl;
+}
