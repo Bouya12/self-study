c9ea9165906daa92b1cfeeacd8f4531c01c87ee8
bouya12 <tsuredurebouya@gmail.com>
2019-09-08 14:18:39 +0900

    Modify ABC140 C - Maximam Value

--- APG4/PastContestQuestions/ABC140/C-MaximamValue.cpp
+++ APG4/PastContestQuestions/ABC140/C-MaximamValue.cpp
@@ -8,15 +8,20 @@ int main() {
   cin >> N;
 
   vector<int> B(N - 1);
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < N - 1; i++) {
     cin >> B.at(i);
   }
 
   vector<int> A(N);
-  for (int i = 0; i < N; i++) {
-    
+  A.at(0) = B.at(0);
+  A.at(N - 1) = B.at(N - 2);
+  for (int i = 1; i < N - 1; i++) {
+    A.at(i) = min(B.at(i), B.at(i - 1));
   }
 
-  int ans;
+  int ans = 0;
+  for (int i = 0; i < N; i++) {
+    ans += A.at(i);
+  }
   cout << ans << endl;
 }
