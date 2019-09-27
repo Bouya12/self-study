81fd191b1b86f0d74312bac7c2d407d274ff5ef9
bouya12 <tsuredurebouya@gmail.com>
2019-09-27 22:55:17 +0900

    Modify Chapter3_5 "Stable Sort"

--- AlgoAndDSfromPGC/Chapter3/Chapter3_5/Stable_Sort.cpp
+++ AlgoAndDSfromPGC/Chapter3/Chapter3_5/Stable_Sort.cpp
@@ -2,34 +2,48 @@
 #define rep(i, n) for(int i = 0; i < (int)(n); i++)
 using namespace std;
 
-string BubbleSort(vector<string> &C, int N) {
+vector<string> BubbleSort(vector<string> C, int N) {
   rep (i, N) {
     for (int j = N - 1; j > i; j--) {
-      int v = C.at(j);
-      if (v < C.at(j - 1)) {
+      string v = C.at(j);
+      if (C.at(j).at(1) < C.at(j - 1).at(1)) {
         C.at(j) = C.at(j - 1);
         C.at(j - 1) = v;
       }
     }
   }
 
-  string stable;
-  return stable;
+  return C;
 }
 
-string SelectionSort(vector<string> &C, int N) {
+vector<string> SelectionSort(vector<string> C, int N) {
   rep (i, N) {
     int minj = i;
-    for (int j = i; j < N - 1; j++) {
-      if (C.at(j) < C.at(minj)) minj = j;
+    for (int j = i; j < N; j++) {
+      if (C.at(j).at(1) < C.at(minj).at(1)) minj = j;
     }
-    int v = C.at(i);
+    string v = C.at(i);
     C.at(i) = C.at(minj);
     C.at(minj) = v;
   }
 
-  string stable;
-  return stable;
+  return C;
+}
+
+bool isStable(vector<string> c1, vector<string> c2, int N) {
+  rep (i, N) {
+    if (c1.at(i) != c2.at(i)) return false;
+  }
+
+  return true;
+}
+
+void Print(vector<string> cards, int N) {
+  rep (i, N) {
+    if (i > 0) cout << " ";
+    cout << cards.at(i);
+  }
+  cout << endl;
 }
 
 int main() {
@@ -39,8 +53,20 @@ int main() {
   vector<string> cards(n);
   rep (i, n) cin >> cards.at(i);
 
-  BubbleSort(cards, n);
-  SelectionSort(cards, n);
+  vector<string> bubble = BubbleSort(cards, n);
+  vector<string> select = SelectionSort(cards, n);
+
+  // Bubble Sort
+  Print(bubble, n);
+  cout << "Stable" << endl;
+
+  // Select Sort
+  Print(select, n);
+  if (isStable(bubble, select, n) == true) {
+    cout << "Stable" << endl;
+  } else {
+    cout << "Not stable" << endl;
+  }
 
   return 0;
 }
