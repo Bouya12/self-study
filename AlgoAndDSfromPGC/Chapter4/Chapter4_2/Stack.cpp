0e4e81f1aaf504e60d8c030e2af39c851a5ed1e7
bouya12 <tsuredurebouya@gmail.com>
2019-09-30 23:55:29 +0900

    Create Chapter4.2 "Stack" ※未完成

---
+++ AlgoAndDSfromPGC/Chapter4/Chapter4_2/Stack.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+using namespace std;
+
+int main() {
+  string S;
+  getline(cin, S);
+
+  vector<char> st;
+  // stackに格納
+
+  // 計算、出力
+
+  return 0;
+}
