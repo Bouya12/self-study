029338dc1790aaf3e8259b54df91142a2f765009
bouya12 <tsuredurebouya@gmail.com>
2019-10-06 23:20:29 +0900

    Modity Chapter4.2 Stack ※入力受け取りが間違っているので要修正

--- AlgoAndDSfromPGC/Chapter4/Chapter4_2/Stack.cpp
+++ AlgoAndDSfromPGC/Chapter4/Chapter4_2/Stack.cpp
@@ -2,14 +2,46 @@
 #define rep(i, n) for(int i = 0; i < (int)(n); i++)
 using namespace std;
 
+int top, S[1000];
+
+void push(int x) {
+  S[++top] = x;
+}
+
+int pop() {
+  top--;
+  return S[top+1];
+}
+
 int main() {
-  string S;
-  getline(cin, S);
+  string in;
+  getline(cin, in);
 
-  vector<char> st;
-  // stackに格納
+  string s = "";
+  for (int i = 0; i < in.size(); i += 2) s += in.at(i);
 
-  // 計算、出力
+  int a, b;
+  top = 0;
+
+  for (int i = 0; i < s.size(); i++) {
+    if (s[i] == '+') {
+      a = pop();
+      b = pop();
+      push(a + b);
+    } else if (s[i] == '-') {
+      b = pop();
+      a = pop();
+      push(a - b);
+    } else if (s[i] == '*') {
+      a = pop();
+      b = pop();
+      push(a * b);
+    } else {
+      push(s[i] - '0');
+    }
+  }
+
+  cout << pop() << endl;
 
   return 0;
 }
