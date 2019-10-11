4b6058f9e8609192b1ca39b0a83327eb06fb2a8b
bouya12 <tsuredurebouya@gmail.com>
2019-10-11 23:54:15 +0900

    Create Section-13 "JestとSuperTestを使用してテストを書こう"

---
+++ VidualStudioCode/徹底解説VidualStudioCpde/vscode-works/api-server/test/app.spec.ts
@@ -0,0 +1,15 @@
+import * as supertest from "supertest";
+import { app } from "../src/app";
+import { response } from "express";
+
+describe("Express server", () => {
+  it("should response the GET method", async (done) => {
+    supertest(app)
+      .get("/")
+      .then((response) => {
+        expect(response.status).toBe(200);
+        expect(response.text).toEqual("Hello, VS Code!!!");
+        done();
+      });
+  });
+});
