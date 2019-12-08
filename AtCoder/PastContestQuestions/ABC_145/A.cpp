78526459a8f4ee572882cd13af80f64176b47657
bouya12 <tsuredurebouya@gmail.com>
2019-12-08 20:57:19 +0900

    Create ABC145

---
+++ AtCoder/PastContestQuestions/ABC_145/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using namespace std;
+
+int main() {
+  int r;
+  cin >> r;
+
+  cout << r * r << endl;
+  return 0;
+}
---
+++ AtCoder/PastContestQuestions/ABC_145/B.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int ans;
+  cout << ans << endl;
+  return 0;
+}
