c1418f9a4fe407516c7ebd7f509cf74c642ecbaf
bouya12 <tsuredurebouya@gmail.com>
2020-01-09 14:53:46 +0900

    Create chapter12 practice12-4

--- SukkiriJava/Chapter12/practice/TangibleAsset.java
+++ SukkiriJava/Chapter12/practice/TangibleAsset.java
@@ -1,8 +1,11 @@
-public abstract class TangibleAsset extends Asset {
-  private final String color;
-  public TangibleAsset(final String name, final int price, final String color) {
+public abstract class TangibleAsset extends Asset implements Tshing {
+  private String color;
+  private double weight;
+  public TangibleAsset( String name,  int price,  String color) {
     super(name, price);
     this.color = color;
   }
   public String getColor() { return this.color; }
+  public String getWeight() { return this.weight; }
+  public String setWeight(double weight) { this.weight = weight; }
 }
