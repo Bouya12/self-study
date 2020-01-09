289c26e5531e459724c8fd8ada0b022fdf5b33b6
bouya12 <tsuredurebouya@gmail.com>
2020-01-09 13:32:07 +0900

    Create chapter12_3

---
+++ SukkiriJava/Chapter12/Character.java
@@ -0,0 +1,12 @@
+public abstract class Character {
+  String name;
+  int hp;
+
+  // “¦‚°‚é
+  public void run() {
+    System.out.println(this.name + "‚Í“¦‚°o‚µ‚½");
+  }
+
+  // í‚¤
+  public abstract void attack(Matango m);
+}
