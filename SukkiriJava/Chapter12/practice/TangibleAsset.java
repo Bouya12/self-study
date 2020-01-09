1c19a1040f1c317eaf24bc3e1b8adcfb0bd560ad
bouya12 <tsuredurebouya@gmail.com>
2020-01-09 14:34:34 +0900

    Create chapter12 practice12-2

--- SukkiriJava/Chapter12/practice/TangibleAsset.java
+++ SukkiriJava/Chapter12/practice/TangibleAsset.java
@@ -1,17 +1,8 @@
-public abstract class TangibleAsset {
-  private String name;
-  private int price;
-  private String color;
-
-  // コンストラクタ
-  public TangibleAsset(String name, int price, String color) {
-    this.name = name;
-    this.price = price;
+public abstract class TangibleAsset extends Asset {
+  private final String color;
+  public TangibleAsset(final String name, final int price, final String color) {
+    super(name, price);
     this.color = color;
   }
-
-  // getterメソッド
-  public String getName() { return this.name; }
-  public int getPrice() { return this.price; }
   public String getColor() { return this.color; }
 }
