e310c3238017da5ba6293cb306e6f8565e97e0f8
bouya12 <tsuredurebouya@gmail.com>
2020-01-10 10:33:38 +0900

    Create chapter15 practice15-4

---
+++ SukkiriJava/Chapter15/Practices/practice15-4/Main.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+/**
+ * Main
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("ƒvƒƒOƒ‰ƒ€‹N“®");
+    try {
+      throw new IOException();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
