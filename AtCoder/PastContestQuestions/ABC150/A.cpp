fdd2c306696cc8301f5edb6ee19e3042214ab451
bouya12 <tsuredurebouya@gmail.com>
2020-01-13 21:54:00 +0900

    Create ABC150 A

---
+++ AtCoder/PastContestQuestions/ABC150/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+using namespace std;
+
+int main() {
+  int k, x;
+  cin >> k >> x;
+
+  if (500 * k <= x) {
+  cout << "Yes" << endl;
+  } else {
+  cout << "Yes" << endl;
+  }
+  
+  return 0;
+}
