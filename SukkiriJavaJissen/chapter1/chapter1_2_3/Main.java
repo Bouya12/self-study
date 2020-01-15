b3fb1cb90f4680f8e99a659317fac15e231963b0
bouya12 <tsuredurebouya@gmail.com>
2020-01-15 22:48:48 +0900

    Create chapter1_2_3

---
+++ SukkiriJavaJissen/chapter1/chapter1_2_3/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    String s1 = "Java programming";
+    System.out.println("•¶š—ñs1‚Ì4•¶š–ÚˆÈ~‚Í" + s1.substring(3));
+    System.out.println("•¶š—ñs1‚Ì4`8•¶š–ÚˆÈ~‚Í" + s1.substring(3, 8));
+  }
+}
