41560d02bafb68aefb7baed7f16866255c6b1ec7
bouya12 <tsuredurebouya@gmail.com>
2020-01-16 23:48:53 +0900

    Create ABC151 A

---
+++ AtCoder/PastContestQuestions/ABC151/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+using namespace std;
+
+int main() {
+  char c;
+  cin >> c;
+
+  int num = c;
+  num++;
+  char ans = num;
+  cout << ans << endl;
+  return 0;
+}
