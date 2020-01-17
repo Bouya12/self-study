8aaef054b9f7e837de7f1a1243f5be6cca5ac2b0
bouya12 <tsuredurebouya@gmail.com>
2020-01-17 23:42:43 +0900

    Create practice2-1

---
+++ SukkiriJavaJissen/chapter2/practice/practice2-1/Main.java
@@ -0,0 +1,18 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+  public static void main(String[] args) {
+    Date now = new Date();
+    Calendar c = Calendar.getInstance();
+    c.setTime(now);
+    int day = c.get(Calendar.DAY_OF_MONTH);
+    day += 100;
+    c.set(Calendar.DAY_OF_MONTH, day);
+    Date future = c.getTime();
+    SimpleDateFormat f = new SimpleDateFormat("¼—ïyyyy”NMMŒdd“ú");
+    String s = f.format(future);
+    System.out.println(s);
+  }
+}
