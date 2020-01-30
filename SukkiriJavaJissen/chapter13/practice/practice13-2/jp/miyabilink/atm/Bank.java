c03a4c6e43a8c4f1546a2b27957638bb1b32469e
bouya12 <tsuredurebouya@gmail.com>
2020-01-30 23:11:25 +0900

    Create practice13-2

---
+++ SukkiriJavaJissen/chapter13/practice/practice13-2/jp/miyabilink/atm/Bank.java
@@ -0,0 +1,21 @@
+package jp.miyabilink.atm;
+
+/**
+ * 銀行クラス。
+ * このクラスは、1つの銀行を表します。
+ */
+public class Bank {
+  /** 銀行名 */
+  String name;
+  /** 住所 */
+  String address;
+  /**
+   * 銀行口座を追加するメソッド
+   * @param ownere 口座名義人
+   * @param initialBalance 初期残高
+   */
+  public void addAcount(String ownere, int initialBalance) { }
+  public static void main(String[] args) {
+    System.out.println("試験用のメソッドです");
+  }
+}
