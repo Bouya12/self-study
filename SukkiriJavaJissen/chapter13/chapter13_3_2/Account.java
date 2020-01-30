8db2161bcf3a13b319966484f7b561364d144cd7
bouya12 <tsuredurebouya@gmail.com>
2020-01-30 23:08:41 +0900

    Create chapter13_3_2

---
+++ SukkiriJavaJissen/chapter13/chapter13_3_2/Account.java
@@ -0,0 +1,11 @@
+package jp.miyabilink.atm;
+import java.io.Serializable;
+
+public class Account {
+  private int balance;
+  private String owner;
+  public int transfer(Bank bank, Account dest, int amount) {
+    // :
+    return 0;
+  }
+}
---
+++ SukkiriJavaJissen/chapter13/chapter13_3_2/Bank.java
@@ -0,0 +1,5 @@
+package jp.miyabilink.atm;
+public class Bank {
+  private String name;
+  private String address;
+}
