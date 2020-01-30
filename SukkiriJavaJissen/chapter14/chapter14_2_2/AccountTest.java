831922667e67412a71e82a97336ead521dc3ae10
bouya12 <tsuredurebouya@gmail.com>
2020-01-30 23:11:39 +0900

    Create chapter14_2_2

---
+++ SukkiriJavaJissen/chapter14/chapter14_2_2/AccountTest.java
@@ -0,0 +1,21 @@
+public class AccountTest {
+  public static void main(String[] args) {
+    testInstratiate();
+    testTransfer();
+  }
+  // 実際にAccountをnewして使ってみるテスト
+  private static void testInstratiate() {
+    System.out.println("無事newできるかテストします");
+    Account a = new Account("ミナト", 30000);
+    if (!"ミナト".equals(a.owner)) {
+      System.out.println("失敗！　名義人がおかしい");
+    }
+    if (30000 != a.balance) {
+      System.out.println("失敗！　残高がおかしい");
+    }
+    System.out.println("テストを終了します");
+  }
+  private static void testTransfer() {
+    // :
+  }
+}
