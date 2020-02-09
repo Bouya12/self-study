983e419f4811bd41ae32ec964e55ba02996330a5
bouya12 <tsuredurebouya@gmail.com>
2020-02-10 00:33:26 +0900

    Modify ABC154 D (AC)

--- AtCoder/PastContestQuestions/ABC154/D.cpp
+++ AtCoder/PastContestQuestions/ABC154/D.cpp
@@ -2,38 +2,24 @@
 #define rep(i, n) for(int64_t i = 0; i < (int64_t)(n); i++)
 using namespace std;
 
-double culcExpectedValue(int64_t number) {
-  double sum = 0;
-  for (int64_t i = 1; i <= number; i++) {
-    sum += i;
-  }
-  double exVal = sum / number;
-  return sum / number;
-}
-
 int main() {
   int64_t n, k;
   cin >> n >> k;
   vector<int64_t> p(n);
   rep (i, n) cin >> p.at(i);
 
-  int maxTotal = 0;
-  int startNo, endNo;
-  for (int i = 0; i < n - k + 1; i++) {
-    int total = 0;
-    for (int j = i; j < i + k; j++) total += p.at(j);
-    if (total > maxTotal) {
-      maxTotal = total;
-      startNo = i;
-      endNo = i + k;
-    }
+  vector<double> exVal(n);
+  rep (i, n) exVal.at(i) = (1 + p.at(i)) / 2.0;
+
+  double total = 0.0;
+  for (int i = 0; i < k; i++) total += exVal.at(i);
+  double maxTotal = total;
+  for (int i = 1; i <= n - k; i++) {
+    total -= (exVal.at(i - 1));
+    total += (exVal.at(i + k - 1));
+    maxTotal = max(maxTotal, total);
   }
 
-  double ans = 0.0;
-  for (int i = startNo; i < endNo; i++) {
-    ans += culcExpectedValue(p.at(i));
-  }
-
-  cout << ans << endl;
+  cout << fixed << setprecision(10) << maxTotal << endl;
   return 0;
 }
