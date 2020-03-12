87a0f2871a79857aa3d728537c3bdc937fa688b1
bouya12 <tsuredurebouya@gmail.com>
2020-03-12 16:15:04 +0900

    Create chapter2 list2-2

---
+++ JavaScript/NodeJsSuperBecome/Chapter2/list2-2/app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+let server = http.createServer((request, response) => {
+  response.end(
+    "<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>"
+  );
+});
+
+server.listen(3000);
