b070581294776ab422675a1b6d499656a22e0b90
bouya12 <tsuredurebouya@gmail.com>
2020-03-12 16:17:31 +0900

    Create node-app from chappter2

--- JavaScript/NodeJsSuperBecome/node-app/app.js
+++ JavaScript/NodeJsSuperBecome/node-app/app.js
@@ -1,7 +1,52 @@
 const http = require("http");
+const fs = require("fs");
+const ejs = require("ejs");
+const url = require("url");
 
-let server = http.createServer((request, response) => {
-  response.end("Hello Node.js!");
-});
+const index_page = fs.readFileSync("./index.ejs", "utf8");
+const other_page = fs.readFileSync("./other.ejs", "utf8");
+const style_css = fs.readFileSync("./style.css", "utf8");
+
+let getFromClient = (request, response) => {
+  let url_parts = url.parse(request.url);
+  switch (url_parts.pathname) {
+    case "/": {
+      let content = ejs.render(index_page, {
+        title: "index",
+        content: "これはテンプレートを使ったサンプルページです。"
+      });
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(content);
+      response.end();
+      break;
+    }
+
+    case "/other": {
+      let content = ejs.render(other_page, {
+        title: "other",
+        content: "これは新しく用意したページです。"
+      });
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(content);
+      response.end();
+      break;
+    }
+
+    case "/style.css": {
+      response.writeHead(200, { "Content-Type": "text/css" });
+      response.write(style_css);
+      response.end();
+      break;
+    }
+
+    default:
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.end("no page...");
+      break;
+  }
+};
+
+let server = http.createServer(getFromClient);
 
 server.listen(3000);
+console.log("Server start!");
