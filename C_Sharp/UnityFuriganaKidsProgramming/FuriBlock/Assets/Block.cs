51072fa77b76c355f11a1caba9d2e126e51a318f
bouya12 <tsuredurebouya@gmail.com>
2020-03-13 18:40:02 +0900

    「ブロックくずし」作成

---
+++ C_Sharp/UnityFuriganaKidsProgramming/FuriBlock/Assets/Block.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+    void OnCollisionEnter (Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
