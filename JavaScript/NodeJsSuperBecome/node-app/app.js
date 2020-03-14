52392a9244c10c60c35f5aa66a6c6192e3787793
bouya12 <tsuredurebouya@gmail.com>
2020-03-14 20:51:36 +0900

    Update node-app from chapter3

--- JavaScript/NodeJsSuperBecome/node-app/app.js
+++ JavaScript/NodeJsSuperBecome/node-app/app.js
@@ -2,42 +2,76 @@ const http = require("http");
 const fs = require("fs");
 const ejs = require("ejs");
 const url = require("url");
+const qs = require("querystring");
 
 const index_page = fs.readFileSync("./index.ejs", "utf8");
-const other_page = fs.readFileSync("./other.ejs", "utf8");
 const style_css = fs.readFileSync("./style.css", "utf8");
 
-let getFromClient = (request, response) => {
-  let url_parts = url.parse(request.url);
+let data = { msg: "no message..." };
+
+const setCookie = (key, value, response) => {
+  let cookie = escape(value);
+  response.setHeader("Set-Cookie", [key + "=" + cookie]);
+};
+
+const getCookie = (key, request) => {
+  let cookie_data =
+    request.headers.cookie !== undefined ? request.headers.cookie : "";
+  let data = cookie_data.split(";");
+  for (const i in data) {
+    if (data[i].trim().startsWith(key + "=")) {
+      let result = data[i].trim().substring(key.length + 1);
+      return unescape(result);
+    }
+  }
+  return "";
+};
+
+const write_index = (request, response) => {
+  let msg = "※伝言を表示します。";
+  let cookie_data = getCookie("msg", request);
+  let content = ejs.render(index_page, {
+    title: "Index",
+    content: msg,
+    data: data,
+    cookie_data: cookie_data
+  });
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.write(content);
+  response.end();
+};
+
+const response_index = (request, response) => {
+  if (request.method === "POST") {
+    let body = "";
+
+    request.on("data", data => {
+      body += data;
+    });
+
+    request.on("end", () => {
+      data = qs.parse(body);
+      setCookie("msg", data.msg, response);
+      write_index(request, response);
+    });
+  } else {
+    write_index(request, response);
+  }
+};
+
+const getFromClient = (request, response) => {
+  let url_parts = url.parse(request.url, true);
+
   switch (url_parts.pathname) {
-    case "/": {
-      let content = ejs.render(index_page, {
-        title: "index",
-        content: "これはテンプレートを使ったサンプルページです。"
-      });
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.write(content);
-      response.end();
+    case "/":
+      response_index(request, response);
       break;
-    }
 
-    case "/other": {
-      let content = ejs.render(other_page, {
-        title: "other",
-        content: "これは新しく用意したページです。"
-      });
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.write(content);
-      response.end();
-      break;
-    }
-
-    case "/style.css": {
+    case "/style.css":
       response.writeHead(200, { "Content-Type": "text/css" });
       response.write(style_css);
       response.end();
       break;
-    }
 
     default:
       response.writeHead(200, { "Content-Type": "text/plain" });
