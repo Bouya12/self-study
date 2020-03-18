a1a9e2ee3a70783297a8071bd12b67a7cefc9701
bouya12 <tsuredurebouya@gmail.com>
2020-03-18 22:39:35 +0900

    Create list5-12

---
+++ JavaScript/NodeJsSuperBecome/Chapter5/list5-12/hello.js
@@ -0,0 +1,31 @@
+// import express from "express";
+const express = require("express");
+const router = express.Router();
+
+const http = require("https");
+const parseString = require("xml2js").parseString;
+
+router.get("/", (req, res, next) => {
+  const opt = {
+    host: "news.google.com",
+    port: 443,
+    path: "/rss?ie=UTF-8&oe=UTF-8&hl=en-US&gl=US&ceid=US:en"
+  };
+  http.get(opt, res2 => {
+    let body = "";
+    res2.on("data", data => {
+      body += data;
+    });
+    res2.on("end", () => {
+      parseString(body.trim(), (err, result) => {
+        const data = {
+          title: "Hello!",
+          content: result.rss.channel[0].item
+        };
+        res.render("hello", data);
+      });
+    });
+  });
+});
+
+module.exports = router;
