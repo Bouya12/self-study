9ebb83d477422048f522db14c37277270dbc9e91
bouya12 <tsuredurebouya@gmail.com>
2020-03-18 22:39:59 +0900

    Create list5-16

---
+++ JavaScript/NodeJsSuperBecome/Chapter5/list5-16/hello.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+
+const mysql = require("mysql");
+
+const mysql_setting = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "my-nodeapp-db"
+};
+
+router.get("/", (req, res, next) => {
+  const connection = mysql.createConnection(mysql_setting);
+  connection.connect();
+  connection.query("SELECT * FROM mydata", (error, results, fields) => {
+    if (error === null) {
+      const data = { title: "mysql", content: results };
+      res.render("hello/index", data);
+    }
+  });
+
+  connection.end();
+});
+
+router.get("/add", (req, res, next) => {
+  const data = {
+    title: "Hello/Add",
+    content: "新しいレコードを入力："
+  };
+  res.render("hello/add", data);
+});
+
+router.post("/add", (req, res, next) => {
+  const name = req.body.name;
+  const mail = req.body.mail;
+  const age = req.body.age;
+  const data = { name, mail, age};
+
+  const connection = mysql.createConnection(mysql_setting);
+  connection.connect();
+
+  connection.query(
+    "INSERT INTO mydata SET ?",
+    data,
+    (error, results, fields) => {
+      if (error === null) {
+        res.redirect("/hello");
+      }
+    }
+  );
+
+  connection.end();
+});
+
+module.exports = router;
