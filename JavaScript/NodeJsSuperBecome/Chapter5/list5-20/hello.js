73538d305be1ab4928f8e3d97cb7d60868c3e60c
bouya12 <tsuredurebouya@gmail.com>
2020-03-18 22:40:25 +0900

    Create list5-20

---
+++ JavaScript/NodeJsSuperBecome/Chapter5/list5-20/hello.js
@@ -0,0 +1,125 @@
+const express = require("express");
+const router = express.Router();
+
+const mysql = require("mysql");
+
+const mysql_setting = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "my-nodeapp-db"
+};
+
+router.get("/", (req, res, next) => {
+  const connection = mysql.createConnection(mysql_setting);
+  connection.connect();
+  connection.query("SELECT * FROM mydata", (error, results, fields) => {
+    if (error === null) {
+      const data = { title: "mysql", content: results };
+      res.render("hello/index", data);
+    }
+  });
+
+  connection.end();
+});
+
+router.get("/add", (req, res, next) => {
+  const data = {
+    title: "Hello/Add",
+    content: "新しいレコードを入力："
+  };
+  res.render("hello/add", data);
+});
+
+router.post("/add", (req, res, next) => {
+  const name = req.body.name;
+  const mail = req.body.mail;
+  const age = req.body.age;
+  const data = { name, mail, age };
+
+  const connection = mysql.createConnection(mysql_setting);
+  connection.connect();
+
+  connection.query(
+    "INSERT INTO mydata SET ?",
+    data,
+    (error, results, fields) => {
+      if (error === null) {
+        res.redirect("/hello");
+      }
+    }
+  );
+
+  connection.end();
+});
+
+router.get("/show", (req, res, next) => {
+  const id = req.query.id;
+
+  const connection = mysql.createConnection(mysql_setting);
+  connection.connect();
+
+  connection.query(
+    "SELECT * FROM mydata WHERE id=?",
+    id,
+    (error, results, fields) => {
+      if (error === null) {
+        const data = {
+          title: "Hello/show",
+          content: `id = ${id} のレコード`,
+          mydata: results[0]
+        };
+        res.render("hello/show", data);
+      }
+    }
+  );
+
+  connection.end();
+});
+
+router.get("/edit", (req, res, next) => {
+  const id = req.query.id;
+
+  const connection = mysql.createConnection(mysql_setting);
+  connection.connect();
+
+  connection.query(
+    "SELECT * FROM mydata WHERE id = ?",
+    id,
+    (error, results, fields) => {
+      if (error === null) {
+        const data = {
+          title: "Hello/edit",
+          content: `id = ${id} のレコード`,
+          mydata: results[0]
+        };
+        res.render("hello/edit", data);
+      }
+    }
+  );
+
+  connection.end();
+});
+
+router.post("/edit", (req, res, next) => {
+  const id = req.body.id;
+  const name = req.body.name;
+  const mail = req.body.mail;
+  const age = req.body.age;
+  const data = { name, mail, age };
+
+  const connection = mysql.createConnection(mysql_setting);
+  connection.connect();
+
+  connection.query(
+    "UPDATE mydata SET ? WHERE id = ?",
+    [data, id],
+    (error, results, fields) => {
+      res.redirect("/hello");
+    }
+  );
+
+  connection.end();
+});
+
+module.exports = router;
