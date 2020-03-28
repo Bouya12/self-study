04d631084d790015ef1c81031371b39dcb50fda0
bouya12 <tsuredurebouya@gmail.com>
2020-03-28 21:44:13 +0900

    Create chapter4

---
+++ furiPHP/Chapter4/chap4_5_1.php
@@ -0,0 +1,4 @@
+<?php
+$order = ["name" => "三浦", "cnt" => 6];
+var_dump($order["name"]);
+var_dump($order["cnt"]);
---
+++ furiPHP/Chapter4/chap4_5_2.php
@@ -0,0 +1,7 @@
+<?php
+$order = [
+    ["name" => "三浦", "cnt" => 6],
+    ["name" => "高野", "cnt" => 8],
+];
+var_dump($order[1]["name"]);
+var_dump($order[1]["cnt"]);
