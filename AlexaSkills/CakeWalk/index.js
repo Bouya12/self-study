102182d417e1fa8134183b7616459869368a1bbd
bouya12 <tsuredurebouya@gmail.com>
2020-05-06 15:54:44 +0900

    Update "5. スキルにメモリ機能を追加する"

--- AlexaSkills/CakeWalk/index.js
+++ AlexaSkills/CakeWalk/index.js
@@ -2,6 +2,7 @@
 // Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
 // session persistence, api calls, and more.
 const Alexa = require('ask-sdk-core');
+const persistenceAdapter = require('ask-sdk-s3-persistence-adapter');
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
@@ -16,15 +17,58 @@ const LaunchRequestHandler = {
             .getResponse();
     }
 };
+const HasBirthdayLaunchRequestHandler = {
+    canHandle(handlerInput) {
+
+        const attributesManager = handlerInput.attributesManager;
+        const sessionAttributes = attributesManager.getSessionAttributes() || {};
+
+        const year = sessionAttributes.hasOwnProperty('year') ? sessionAttributes.year : 0;
+        const month = sessionAttributes.hasOwnProperty('month') ? sessionAttributes.month : 0;
+        const day = sessionAttributes.hasOwnProperty('day') ? sessionAttributes.day : 0;
+
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest' && year && month && day;
+
+    },
+    handle(handlerInput) {
+
+        const attributesManager = handlerInput.attributesManager;
+        const sessionAttributes = attributesManager.getSessionAttributes() || {};
+
+        const year = sessionAttributes.hasOwnProperty('year') ? sessionAttributes.year : 0;
+        const month = sessionAttributes.hasOwnProperty('month') ? sessionAttributes.month : 0;
+        const day = sessionAttributes.hasOwnProperty('day') ? sessionAttributes.day : 0;
+
+        // TODO:: 設定APIを使って現在の日付を取得し、ユーザーの誕生日までの日数を計算します
+        // TODO:: ユーザーの誕生日当日におめでとうと言います
+
+        const speakOutput = `おかえりなさい。Y歳の誕生日まであとX日です。`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
 const CaptureBirthdayIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'CaptureBirthdayIntent';
     },
-    handle(handlerInput) {
+    async handle(handlerInput) {
         const year = Alexa.getSlotValue(handlerInput.requestEnvelope, 'year');
         const month = Alexa.getSlotValue(handlerInput.requestEnvelope, 'month');
         const day = Alexa.getSlotValue(handlerInput.requestEnvelope, 'day');
+
+        const attributesManager = handlerInput.attributesManager;
+
+        const birthdayAttributes = {
+            "year" : year,
+            "month" : month,
+            "day" : day
+        };
+        attributesManager.setPersistentAttributes(birthdayAttributes);
+        await attributesManager.savePersistentAttributes();
+
         const speakOutput = `ありがとうございます。誕生日は${year}年${month}月${day}日ですね。`;
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -106,17 +150,39 @@ const ErrorHandler = {
     }
 };
 
+const LoadBirthdayInterceptor = {
+    async process(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const sessionAttributes = await attributesManager.getPersistentAttributes() || {};
+
+        const year = sessionAttributes.hasOwnProperty('year') ? sessionAttributes.year : 0;
+        const month = sessionAttributes.hasOwnProperty('month') ? sessionAttributes.month : 0;
+        const day = sessionAttributes.hasOwnProperty('day') ? sessionAttributes.day : 0;
+
+        if (year && month && day) {
+            attributesManager.setSessionAttributes(sessionAttributes);
+        }
+    }
+};
+
 // The SkillBuilder acts as the entry point for your skill, routing all request and response
 // payloads to the handlers above. Make sure any new handlers or interceptors you've
 // defined are included below. The order matters - they're processed top to bottom.
 exports.handler = Alexa.SkillBuilders.custom()
+    .withPersistenceAdapter(
+    new persistenceAdapter.S3PersistenceAdapter({bucketName:process.env.S3_PERSISTENCE_BUCKET})
+    )
     .addRequestHandlers(
+        HasBirthdayLaunchRequestHandler,
         LaunchRequestHandler,
         CaptureBirthdayIntentHandler,
         HelpIntentHandler,
         CancelAndStopIntentHandler,
         SessionEndedRequestHandler,
         IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    )
+    .addRequestInterceptors(
+        LoadBirthdayInterceptor
     )
     .addErrorHandlers(
         ErrorHandler,
