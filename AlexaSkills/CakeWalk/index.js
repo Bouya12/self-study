4bfdb753c524d3ac282d824ed164721d6f0a8d76
bouya12 <tsuredurebouya@gmail.com>
2020-05-08 17:18:05 +0900

    Update "6. Alexa設定APIを使う"

--- AlexaSkills/CakeWalk/index.js
+++ AlexaSkills/CakeWalk/index.js
@@ -30,19 +30,50 @@ const HasBirthdayLaunchRequestHandler = {
         return handlerInput.requestEnvelope.request.type === 'LaunchRequest' && year && month && day;
 
     },
-    handle(handlerInput) {
+    async handle(handlerInput) {
 
+        const serviceClientFactory = handlerInput.serviceClientFactory;
+        const deviceId = Alexa.getDeviceId(handlerInput.requestEnvelope);
         const attributesManager = handlerInput.attributesManager;
         const sessionAttributes = attributesManager.getSessionAttributes() || {};
 
         const year = sessionAttributes.hasOwnProperty('year') ? sessionAttributes.year : 0;
         const month = sessionAttributes.hasOwnProperty('month') ? sessionAttributes.month : 0;
         const day = sessionAttributes.hasOwnProperty('day') ? sessionAttributes.day : 0;
+        let userTimeZone;
+        try {
+            const upsServiceClient = serviceClientFactory.getUpsServiceClient();
+            userTimeZone = await upsServiceClient.getSystemTimeZone(deviceId);
+        } catch (error) {
+            if (error.name !== 'ServiceError') {
+                return handlerInput.responseBuilder.speak("サービスへの接続がうまく行きませんでした。").getResponse();
+            }
+            console.log('error', error.message);
+        }
 
-        // TODO:: 設定APIを使って現在の日付を取得し、ユーザーの誕生日までの日数を計算します
-        // TODO:: ユーザーの誕生日当日におめでとうと言います
+        // 現在の日付と時刻を取得します
+        const currentDateTime = new Date(new Date().toLocaleString("ja-JP", {timeZone: userTimeZone}));
 
-        const speakOutput = `おかえりなさい。Y歳の誕生日まであとX日です。`;
+        // 日数計算の結果に影響するため、日付から時刻を取り除きます
+        const currentDate = new Date(currentDateTime.getFullYear(), currentDateTime.getMonth(), currentDateTime.getDate());
+        const currentYear = currentDate.getFullYear();
+
+        // 次の誕生日を取得します
+        let nextBirthday = Date.parse(`${month} ${day}, ${currentYear}`);
+
+        // 現在の日付が誕生日よりも後の場合、nextBirthdayに1年足します
+        if (currentDate.getTime() > nextBirthday) {
+            nextBirthday = Date.parse(`${month} ${day}, ${currentYear + 1}`);
+        }
+
+        const oneDay = 24*60*60*1000;
+
+        // デフォルトのspeechTextを「X歳の誕生日、おめでとうございます」に置き換えます。
+        let speakOutput = `${currentYear - year}歳のお誕生日、おめでとうございます！`;
+        if (currentDate.getTime() !== nextBirthday) {
+            const diffDays = Math.round(Math.abs((currentDate.getTime() - nextBirthday)/oneDay));
+            speakOutput = `おかえりなさい、${currentYear - year}歳のお誕生日まで、残り${diffDays}日です。`
+        }
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -169,6 +200,7 @@ const LoadBirthdayInterceptor = {
 // payloads to the handlers above. Make sure any new handlers or interceptors you've
 // defined are included below. The order matters - they're processed top to bottom.
 exports.handler = Alexa.SkillBuilders.custom()
+    .withApiClient(new Alexa.DefaultApiClient())
     .withPersistenceAdapter(
     new persistenceAdapter.S3PersistenceAdapter({bucketName:process.env.S3_PERSISTENCE_BUCKET})
     )
